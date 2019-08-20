efb4ec250f6c0b9eeefcf5b6670a137bcf00cc00
Clément Baumann <clement.baumann@hotmail.fr>
2019-08-20 17:24:22 +0200

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -25,11 +25,20 @@ op = optimizer.minimize(loss)
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(10000):
-        get_next_images, get_next_labels = mnist.train.next_batch(10000)
+    for i in range(3000):
+        get_next_images, get_next_labels = mnist.train.next_batch(3000)
         sess.run(loss, feed_dict={entries: get_next_images, labels: get_next_labels})
         sess.run(op, feed_dict={entries: get_next_images, labels: get_next_labels})
     outs = sess.run(tf.sigmoid(outs), feed_dict={entries: get_test_images})
+    ok = 0
+    total = 0
+    for i in range(len(outs)):
+    maxOut = np.argmax(outs[i])
+    maxTest = np.argmax(get_test_labels[i])
+    if maxOut == maxTest:
+        ok += 1.
+    total += 1.
+print("Accuracy : %.2f%%" % ((ok * 1000 / total) / 10.0))
 ok = 0.
 total = 0.
 for i in range(len(outs)):
