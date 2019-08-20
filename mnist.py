fc3879f97ca99de48e7390a9ce8f4693ccab5dbb
Clement Baumann <clement.baumann@hotmail.fr>
2019-08-20 17:49:38 +0200

    mnist

--- mnist.py
+++ mnist.py
@@ -2,8 +2,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
 
+print('Downloading datasets...')
 # get mnist dataset
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+print('Datasets downloaded')
 
 # get next 100 images
 get_next_images, get_next_labels = mnist.train.next_batch(100)
