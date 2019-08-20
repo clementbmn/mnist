534152027779a0b417dfa8f7c4f41b0a12489c5f
Clément Baumann <clement.baumann@hotmail.fr>
2019-08-20 17:17:20 +0200

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -20,13 +20,13 @@ outs = tf.layers.dense(hidden2, 10)
 
 labels = tf.placeholder(tf.float32, [None, 10])
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=outs, labels=labels), axis=0)
-optimizer = tf.train.AdamOptimizer(0.0002)
+optimizer = tf.train.AdamOptimizer(0.001)
 op = optimizer.minimize(loss)
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     for i in range(5000):
-        get_next_images, get_next_labels = mnist.train.next_batch(1000)
+        get_next_images, get_next_labels = mnist.train.next_batch(5000)
         sess.run(loss, feed_dict={entries: get_next_images, labels: get_next_labels})
         sess.run(op, feed_dict={entries: get_next_images, labels: get_next_labels})
     outs = sess.run(tf.sigmoid(outs), feed_dict={entries: get_test_images})
